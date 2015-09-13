f74ddc52eb1d2b2ecbdd158c2128085dee50093b
Marco Dalcò <mdalco@mdsol.com>
2015-09-13 02:23:51 +0100

    Removed useless constructor.

--- montezuma-cases/src/main/java/analysethis/utils/math/CurrencyUtils.java
+++ montezuma-cases/src/main/java/analysethis/utils/math/CurrencyUtils.java
@@ -11,10 +11,6 @@ import java.util.Locale;
 public class CurrencyUtils implements Serializable {
 	private static final long	serialVersionUID	= 3502868223192370152L;
 
-	public CurrencyUtils() {
-		System.out.println("Currency Utils  instantiated");
-	}
-
 	private static final DecimalFormatSymbols	DEFAULT_LOCALE									= DecimalFormatSymbols.getInstance(Locale.US);
 	private static final Currency							DEFAULT_CURRENCY								= DEFAULT_LOCALE.getCurrency();
 	private static final NumberFormat					DEFAULT_CURRENCY_NUMBER_FORMAT	= new DecimalFormat("#0.00", DEFAULT_LOCALE);
